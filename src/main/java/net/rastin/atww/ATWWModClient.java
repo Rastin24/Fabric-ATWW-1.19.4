937d5e94b38a41428d60b02d5ad6a9db4c3aa4fc
Rastin24 <rastin24offical@gmail.com>
2023-05-19 14:37:18 +0300

    Initial commit

---
+++ src/main/java/net/rastin/atww/ATWWModClient.java
@@ -0,0 +1,10 @@
+package net.rastin.atww;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ATWWModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
